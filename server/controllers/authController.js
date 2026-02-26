1190f10b7da3e43aaeb9fd05eb2587b16faedd16
Naveen <navindena2432000@gmail.com>
2026-02-26 15:05:54 +0530

    register and login api added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,8 +17,58 @@ export const register = async (req,res) => {
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        
+
+        const user = new userModel({name, email, password: hashedPassword});
+        await user.save();
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 
+            'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
     }catch(error){
         res.json( {success: false, message:"something went wrong"})
+    }
+}
+
+
+export const login = async(req,res)=> {
+    const {email,password} = req.body;
+    
+    if(!email || !password){
+        return res.json({success: false, message:"Email and Password are required"})
+    }
+
+    try{
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            res.json({success:false, message:'Invalid email'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.json({success:false, message:"Invalid password"})
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 
+            'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({success: true});
+        
+
+    }catch(error){
+
     }
 }
