2c18f1a6c9b2123e7268341a6fbabd89c0198746
Naveen <navindena2432000@gmail.com>
2026-02-26 15:31:54 +0530

    routes added for apis

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -72,3 +72,20 @@ export const login = async(req,res)=> {
 
     }
 }
+
+export const logout = async (req,res)=>{
+
+    try{
+        res.clearCookie('token',{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 
+            'none' : 'strict'
+        })
+
+        return res.josn({success: true, message: "Logged out"})
+        
+    }catch(error){
+        return res.json({ success:false, message:error.message });
+    }
+}
