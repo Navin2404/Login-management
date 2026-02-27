a5eabfee5fc138c5b022b3a738529f07c33e9e03
Naveen <navindena2432000@gmail.com>
2026-02-27 21:06:33 +0530

    Login form created

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -3,7 +3,7 @@ import lock_icon from './lock_icon.svg'
 import logo from './logo.svg'
 import mail_icon from './mail_icon.svg'
 import person_icon from './person_icon.svg'
-import hand_wave from './hand_wave.png'
+import hands_wave from './hands_wave.png'
 import header_img from './header_img.png'
 
 export const assets = {
@@ -12,6 +12,6 @@ export const assets = {
     logo,
     mail_icon,
     person_icon,
-    hand_wave,
+    hands_wave,
     header_img
 }
