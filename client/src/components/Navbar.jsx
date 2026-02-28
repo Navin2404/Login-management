5e3424dda1b5c8ba7d4c63faf79dfd1b9f6fe029
Naveen <navindena2432000@gmail.com>
2026-02-28 12:17:59 +0530

    client side form completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
 
-      <img src={assets.hands_wave} alt="" className='w-28 sm:w-32'/>
+      <img src={assets.logo} alt="" className='w-28 sm:w-32'/>
       <button onClick = {()=>navigate('/login')}
       className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-grey-800 hover:bg-gray-500'>Login<img src={assets.arrow_icon} alt="" /></button>
 
